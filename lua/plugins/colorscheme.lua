c1340027ecae8a1c3851dea67eb5a6517d7456b4
GrigoreAlexandru-Arch <mihaieleandru412@gmail.com>
2023-10-23 18:04:54 +0300

    Changed colorscheme to github-colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,11 @@
 return {
-	{
-		"rose-pine/neovim",
-		config = function()
-			require("rose-pine").setup({
-				disable_italics = true,
-				disable_float_background = true,
-				disable_background = true,
-			})
-		end,
-	},
-
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "rose-pine",
-		},
-	},
+    {
+        { 'projekt0n/github-nvim-theme' }
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "github_dark_high_contrast",
+        },
+    },
 }
