ef66db8a052d2dafb69c7013dfb8d78244996601
GrigoreAlexandru-Arch <mihaieleandru412@gmail.com>
2026-06-27 18:35:03 +0300

    Update lazyvim add win32yank-wsl

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -24,3 +24,18 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
+
+if vim.fn.has("wsl") == 1 then
+	vim.g.clipboard = {
+		name = "win32yank-wsl",
+		copy = {
+			["+"] = "win32yank.exe -i --crlf",
+			["*"] = "win32yank.exe -i --crlf",
+		},
+		paste = {
+			["+"] = "win32yank.exe -o --lf",
+			["*"] = "win32yank.exe -o --lf",
+		},
+		cache_enabled = 0,
+	}
+end
